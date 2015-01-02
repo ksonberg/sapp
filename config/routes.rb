7099e165b753f52634fb2ec2a208ecc57afeeaed
Ken Sonberg <ken.sonberg@gmail.com>
2015-01-02 13:54:59 -0500

    loginout

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Rails.application.routes.draw do
-  get 'users/new'
+=begin
+  get 'sessions/new'
 
+  get 'users/new'
+=end
   root             'static_pages#home'
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
 
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   resources :users
-  
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
