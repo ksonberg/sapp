6e748d6b376963ede8779d813fb8b4bb6827cb4d
Ken Sonberg <ken.sonberg@gmail.com>
2015-01-23 16:22:53 -0500

    Add acount activations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150116223935) do
+ActiveRecord::Schema.define(version: 20150121220524) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -21,6 +21,9 @@ ActiveRecord::Schema.define(version: 20150116223935) do
     t.string   "password_digest"
     t.string   "remember_digest"
     t.boolean  "admin"
+    t.string   "activation_digest"
+    t.boolean  "activated",         default: false
+    t.datetime "activated_at"
   end
 
 end
