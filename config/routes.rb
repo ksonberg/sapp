166568d8183ce40fff48b5eb1a650c86de171abe
Ken Sonberg <ken.sonberg@gmail.com>
2015-01-23 17:00:02 -0500

    Add pw resets

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
 =begin
   get 'sessions/new'
 
@@ -15,6 +19,7 @@ Rails.application.routes.draw do
   delete 'logout'  => 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
